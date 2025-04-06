1bf84635f9d7c5c89921b394394f0e31114f8ff9
Modala swathi <swathimodala26@gmail.com>
2025-04-06 22:48:22 +0530

    Deleting demo

--- my_club/members/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Destination(models.Model):
-  name=models.CharField(max_length=200)
-  img=models.ImageField(upload_to='destination_images/')
-  dest=models.CharField(max_length=200)
-  price=models.IntegerField()
